f3454410b4a832ac9ce46e35beaf89c74bdb5fca
darryl1975 <darryl1975@hotmail.com>
2025-01-14 13:35:53 +0800

    added vttp5a-paf-day25

---
+++ vttp5a-paf-day25/src/main/java/sg/edu/nus/iss/vttp5a_paf_day25/Vttp5aPafDay25Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5a_paf_day25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5aPafDay25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5aPafDay25Application.class, args);
+	}
+
+}
---
+++ vttp5a-paf-day25/src/test/java/sg/edu/nus/iss/vttp5a_paf_day25/Vttp5aPafDay25ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5a_paf_day25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5aPafDay25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
