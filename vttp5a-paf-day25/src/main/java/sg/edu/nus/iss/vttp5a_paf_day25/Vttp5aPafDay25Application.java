1d84efe8a3563c10fcb397a46e1804e1a07c6e66
darryl1975 <darryl1975@hotmail.com>
2025-01-14 14:23:22 +0800

    vttp5a-paf-day25

--- vttp5a-paf-day25/src/main/java/sg/edu/nus/iss/vttp5a_paf_day25/Vttp5aPafDay25Application.java
+++ vttp5a-paf-day25/src/main/java/sg/edu/nus/iss/vttp5a_paf_day25/Vttp5aPafDay25Application.java
@@ -1,13 +1,29 @@
 package sg.edu.nus.iss.vttp5a_paf_day25;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import sg.edu.nus.iss.vttp5a_paf_day25.service.MessagePoller;
+
 @SpringBootApplication
-public class Vttp5aPafDay25Application {
+public class Vttp5aPafDay25Application implements CommandLineRunner {
+
+	// slide 11
+	@Autowired
+	private MessagePoller messagePoller;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Vttp5aPafDay25Application.class, args);
+
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		messagePoller.start();
 	}
 
 }
